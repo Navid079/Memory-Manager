21b2665bc37347c51546744e2bc85a2121c11266
Navid <navid.naseri.079@gmail.com>
2022-01-23 23:13:07 +0330

    Page model added

---
+++ packages/models/Page.py
@@ -0,0 +1,22 @@
+class Page:
+
+  entered = -1
+  referenced = -1
+
+  def __init__(self, label, references):
+    self.label = label
+    self.references = references
+
+  def enter(self, time):
+    self.entered = time
+
+  def reference(self, time):
+    self.referenced = time
+
+  def is_referenced(self, time):
+    return time in self.references
+
+  def next_reference(self, time):
+    refs = list(filter(lambda ref: ref > time, self.references))
+    if refs: return refs[0]
+    else: return -1
